750c9cbbb2cbde3049dcb87818fe12213517dda6
auxeno <alex1goddard@gmail.com>
2025-10-23 21:09:59 +0100

    Update default config to use a larger model

--- fable/config.py
+++ fable/config.py
@@ -9,25 +9,25 @@ from fable.data.tokenize import get_vocabulary_size
 
 @dataclass(frozen=True)
 class GPTConfig:
-    num_layers: int = 4
+    num_layers: int = 6
     """Number of transformer decoder layers."""
 
-    embed_dim: int = 128
+    embed_dim: int = 384
     """Dimensionality of input embeddings."""
 
-    num_heads: int = 4
+    num_heads: int = 6
     """Number of attention heads."""
 
     vocab_size: int = get_vocabulary_size()
     """Size of the vocabulary."""
 
-    max_seq_len: int = 128
+    max_seq_len: int = 192
     """Maximum sequence length."""
 
-    dropout_rate: float = 0.1
+    dropout_rate: float = 0.2
     """Dropout rate to apply after embeddings and transformer blocks."""
 
-    learning_rate: float = 2.5e-4
+    learning_rate: float = 3e-4
     """Learning rate for the optimizer."""
 
     batch_size: int = 64
