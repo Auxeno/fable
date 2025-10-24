d171b693be98192863d3a1f27982ba7c1137bc2c
auxeno <alex1goddard@gmail.com>
2025-10-24 18:15:23 +0100

    Add more functions and classes to module __init__.py

--- fable/__init__.py
+++ fable/__init__.py
@@ -2,6 +2,10 @@
 Fable top-level package namespace and exports.
 """
 
+from fable.checkpoint import load, save
+from fable.config import GPTConfig
 from fable.generate import generate_text
+from fable.model import GPT
+from fable.train import train
 
-__all__ = ["generate_text"]
+__all__ = ["generate_text", "GPT", "GPTConfig", "load", "save", "train"]
