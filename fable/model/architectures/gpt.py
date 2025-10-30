2b5a35be704b8d027672a259e41c117232038439
auxeno <alex1goddard@gmail.com>
2025-10-30 20:26:58 +0000

    Refactor model structure

--- fable/model/architectures/gpt.py
+++ fable/model/architectures/gpt.py
@@ -10,10 +10,65 @@ from flax import nnx
 from jax.nn.initializers import truncated_normal
 
 from fable.config import GPTConfig
-from fable.model.dropout import Dropout
-from fable.model.normalize import LayerNorm
-from fable.model.position import sinusoidal_embeddings
-from fable.model.transformer import Transformer
+from fable.model.core.attention import MultiHeadAttention
+from fable.model.core.dropout import Dropout
+from fable.model.core.feed_forward import FeedForward
+from fable.model.core.normalize import LayerNorm
+from fable.model.core.position import sinusoidal_embeddings
+
+
+class Transformer(nnx.Module):
+    """GPT-style transformer decoder block module."""
+
+    def __init__(
+        self,
+        dim: int,
+        num_heads: int,
+        dropout_rate: float,
+        *,
+        hidden_mult: int = 4,
+        use_bias: bool = False,
+        init: Callable = truncated_normal(stddev=0.02),
+        dtype: jnp.dtype = jnp.float32,
+        rngs: nnx.Rngs = nnx.Rngs(params=0, dropout=1),
+    ) -> None:
+        self.layer_norm_1 = LayerNorm(dim, dtype=dtype)
+        self.layer_norm_2 = LayerNorm(dim, dtype=dtype)
+
+        self.dropout_1 = Dropout(rate=dropout_rate, rngs=rngs)
+        self.dropout_2 = Dropout(rate=dropout_rate, rngs=rngs)
+
+        self.attention = MultiHeadAttention(
+            dim=dim,
+            num_heads=num_heads,
+            init=init,
+            dtype=dtype,
+            rngs=rngs,
+        )
+
+        self.feed_forward = FeedForward(
+            dim=dim,
+            hidden_mult=hidden_mult,
+            use_bias=use_bias,
+            init=init,
+            dtype=dtype,
+            rngs=rngs,
+        )
+
+    def __call__(self, x: jax.Array) -> jax.Array:
+        residual = x
+        x = self.layer_norm_1(x)
+        x = self.attention(x, causal=True)
+        x = self.dropout_1(x)
+        x = x + residual
+        x = self.layer_norm_2(x)
+
+        residual = x
+        x = self.feed_forward(x)
+        x = self.dropout_2(x)
+        x = x + residual
+
+        return x
 
 
 class GPT(nnx.Module):
